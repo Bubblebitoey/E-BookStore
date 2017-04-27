5197ecc6d9524060e72d085b68568c14c7e1c191
Soraya Saenna <bubblebitoey@gmail.com>
2017-04-27 16:16:36 +0700

    helper method

---
+++ app/src/main/java/com/example/bubblebitoey/sw_specebook/presenter/PassingActivity.java
@@ -0,0 +1,23 @@
+package com.example.bubblebitoey.sw_specebook.presenter;
+
+import android.content.Context;
+import android.content.Intent;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * Created by bubblebitoey on 4/27/2017 AD.
+ */
+
+public class PassingActivity {
+	public static void newActivity(Map<String, Serializable> map, Context from, Class to) {
+		//Create intent
+		Intent intent = new Intent(from, to);
+		for (String key : map.keySet()) {
+			intent.putExtra(key, map.get(key));
+		}
+		//Start details activity
+		from.startActivity(intent);
+	}
+}
