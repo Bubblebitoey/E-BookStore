808b1dc718974181bd4c62b6c93dd3e2862952e2
Soraya Saenna <bubblebitoey@gmail.com>
2017-04-27 14:12:38 +0700

    clean code

--- app/src/main/java/com/example/bubblebitoey/sw_specebook/view/MainActivity.java
+++ app/src/main/java/com/example/bubblebitoey/sw_specebook/view/MainActivity.java
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity implements View {
 	private GridAdapter gridAdapter;
 	private ProgressBar progressBar;
 	private EditText searchBar;
-
+	
 	private Books books;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,14 +36,14 @@ public class MainActivity extends AppCompatActivity implements View {
 		progressBar = (ProgressBar) findViewById(R.id.progress);
 		searchBar = (EditText) findViewById(R.id.search_bar);
 		books = new Books();
-
+		
 		search(false);
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN); // disable auto appear keyboard
-
+		
 		store = new MockupStore().setView(this);
 		store.loadBook();
 	}
-
+	
 	@Override
 	public void updateData(final Book book) {
 		books.add(book);
@@ -54,27 +54,27 @@ public class MainActivity extends AppCompatActivity implements View {
 			}
 		});
 	}
-
+	
 	@Override
 	public void updateData(Books books) {
 		books.addAll(books);
 	}
-
+	
 	@Override
 	public synchronized void setMaxProgress(int max) {
 		progressBar.setMax(max);
 	}
-
+	
 	@Override
 	public synchronized void updateProgress(int current) {
 		progressBar.setProgress(current);
 	}
-
+	
 	@Override
 	public void removeProgress() {
 		progressBar.setVisibility(android.view.View.INVISIBLE);
 	}
-
+	
 	@Override
 	public void search(boolean enable) {
 		searchBar.setVisibility(enable ? android.view.View.VISIBLE: android.view.View.INVISIBLE);
@@ -83,31 +83,28 @@ public class MainActivity extends AppCompatActivity implements View {
 			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 				// do nothing
 			}
-
+			
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 				// do nothing
 			}
-
+			
 			@Override
 			public void afterTextChanged(Editable s) {
 				if (!s.toString().isEmpty()) {
-					if (Character.isDigit(s.toString().charAt(0))) {
-						gridAdapter.setFilter(Store.OperationType.YEAR);
-					} else {
-						gridAdapter.setFilter(Store.OperationType.TITLE);
-					}
+					if (Character.isDigit(s.toString().charAt(0))) gridAdapter.setFilter(Store.OperationType.YEAR);
+					else gridAdapter.setFilter(Store.OperationType.TITLE);
 				}
 				gridAdapter.getFilter().filter(s.toString());
 			}
 		});
 	}
-
+	
 	@Override
 	public void sort(Comparator<Book> compare) {
 		gridAdapter.sort(compare);
 	}
-
+	
 	@Override
 	public void filter(Store.OperationType type, String text) {
 		gridAdapter.setFilter(type);
