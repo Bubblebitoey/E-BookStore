abf7f375899c24a3b71744a58e6d3428741c073b
kamontat <kamontat_c@hotmail.com>
2017-04-27 21:15:04 +0700

    Book builder

---
+++ app/src/main/java/com/example/bubblebitoey/sw_specebook/api/BookBuilder.java
@@ -0,0 +1,30 @@
+package com.example.bubblebitoey.sw_specebook.api;
+
+import com.example.bubblebitoey.sw_specebook.model.Book;
+import com.example.bubblebitoey.sw_specebook.model.NullBook;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by kamontat on 4/27/2017 AD.
+ */
+
+public class BookBuilder {
+	public static Book createBook(String id, String title, String link, double price, String year) {
+		return new Book(id, title, link, price, year);
+	}
+	
+	public static Book createBook(JSONObject object) {
+		try {
+			String id = object.getString("id");
+			String title = object.getString("title");
+			String link = object.getString("img_url");
+			double price = object.getDouble("price");
+			String year = object.getString("pub_year");
+			return BookBuilder.createBook(id, title, link, price, year);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return new NullBook();
+	}
+}
