4dd6965c26a1573c6f9120701de415a439f5c539
kamontat <kamontat_c@hotmail.com>
2017-04-29 13:23:44 +0700

    renovate factory

--- app/src/main/java/com/example/bubblebitoey/sw_specebook/api/factory/UserFactory.java
+++ app/src/main/java/com/example/bubblebitoey/sw_specebook/api/factory/UserFactory.java
@@ -1,5 +1,6 @@
 package com.example.bubblebitoey.sw_specebook.api.factory;
 
+import android.support.annotation.Nullable;
 import com.example.bubblebitoey.sw_specebook.model.mock.MockupUser;
 import com.example.bubblebitoey.sw_specebook.model.raw.User;
 import com.example.bubblebitoey.sw_specebook.model.real.RealUser;
@@ -18,18 +19,21 @@ public class UserFactory {
 		return factory;
 	}
 	
-	public User createMockUser() {
+	public void createMockUser() {
 		if (user == null) user = new MockupUser();
-		return user;
 	}
 	
-	public User createRealUser(String name) {
+	public void createRealUser(String name) {
 		if (user == null) user = new RealUser(name);
-		return user;
 	}
 	
 	// create first
+	@Nullable
 	public User getUser() {
 		return user;
+	}
+	
+	public void logout() {
+		user = null;
 	}
 }
