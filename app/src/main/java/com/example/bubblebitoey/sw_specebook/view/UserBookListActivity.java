adafd91382080eeeddb3528c6ae3352c1559309b
kamontat <kamontat_c@hotmail.com>
2017-04-29 13:38:49 +0700

    change name

---
+++ app/src/main/java/com/example/bubblebitoey/sw_specebook/view/UserBookListActivity.java
@@ -0,0 +1,18 @@
+package com.example.bubblebitoey.sw_specebook.view;
+
+import android.os.Bundle;
+import com.example.bubblebitoey.sw_specebook.model.raw.User;
+import com.example.bubblebitoey.sw_specebook.presenter.UserBooksPresenter;
+
+public class UserBookListActivity extends BookListActivity {
+	
+	public UserBookListActivity() {
+		super(new UserBooksPresenter());
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		User user = (User) getIntent().getParcelableExtra("user");
+		super.onCreate(savedInstanceState);
+	}
+}
