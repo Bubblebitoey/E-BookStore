5a8c186cd70932050eae46ce195f17247a5931a0
kamontat <kamontat_c@hotmail.com>
2017-04-29 12:53:18 +0700

    move class

--- app/src/main/java/com/example/bubblebitoey/sw_specebook/api/view/PassingActivity.java
+++ app/src/main/java/com/example/bubblebitoey/sw_specebook/api/view/PassingActivity.java
@@ -1,17 +1,17 @@
-package com.example.bubblebitoey.sw_specebook.presenter;
+package com.example.bubblebitoey.sw_specebook.api.view;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
 
-import java.io.Serializable;
 import java.util.*;
 
 /**
  * Created by bubblebitoey on 4/27/2017 AD.
  */
 public class PassingActivity {
-	public static void newActivity(Map<String, Serializable> map, Context from, Class to) {
+	public static void newActivity(Map<String, Parcelable> map, Context from, Class to) {
 		//Create intent
 		Intent intent = makeIntent(map).setClass(from, to);
 		//Start details activity
@@ -23,7 +23,7 @@ public class PassingActivity {
 		from.startActivity(new Intent(from, to));
 	}
 	
-	public static void newActivityWithResult(int code, Map<String, Serializable> map, Activity from, Class to) {
+	public static void newActivityWithResult(int code, Map<String, Parcelable> map, Activity from, Class to) {
 		//Create intent
 		Intent intent = makeIntent(map).setClass(from, to);
 		//Start details activity
@@ -34,7 +34,7 @@ public class PassingActivity {
 		from.startActivityForResult(new Intent(from, to), code);
 	}
 	
-	private static Intent makeIntent(Map<String, Serializable> map) {
+	private static Intent makeIntent(Map<String, Parcelable> map) {
 		Intent intent = new Intent();
 		for (String key : map.keySet()) {
 			intent.putExtra(key, map.get(key));
