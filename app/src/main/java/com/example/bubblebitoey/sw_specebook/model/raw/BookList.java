5df3728cf254b45f75a3536b3732628f33691774
kamontat <kamontat_c@hotmail.com>
2017-04-29 13:22:46 +0700

    change name

--- app/src/main/java/com/example/bubblebitoey/sw_specebook/model/raw/BookList.java
+++ app/src/main/java/com/example/bubblebitoey/sw_specebook/model/raw/BookList.java
@@ -8,7 +8,7 @@ import com.example.bubblebitoey.sw_specebook.model.Books;
  * Created by kamontat on 4/28/2017 AD.
  */
 
-public interface Booklist {
+public interface BookList {
 	void addNewBook(Book book);
 	
 	void addAll(Books books);
